e135492602b3215cd401124ffffd519f461fce71
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2025-11-24 06:20:25 +0000

    modify build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,5 @@
-// build.rs
+// build.rs - 簡化版本
 fn main() {
-    // 強制連結數學庫
+    // 只連結數學庫，不強制靜態
     println!("cargo:rustc-link-lib=m");
-    
-    // 針對 musl 目標的設定
-    let target = std::env::var("TARGET").unwrap();
-    if target.contains("musl") {
-        println!("cargo:rustc-link-lib=m");
-        println!("cargo:rustc-link-arg=-static");
-    }
 }
